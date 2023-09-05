240af29f562c064aa3799c52b43af03ab5bfaeed
giuliohome <giuliohome@users.noreply.github.com>
2023-09-05 20:46:12 +0000

    MainMenu should be visible for Reporting

--- src/app/reporting/rpt-context.tsx
+++ src/app/reporting/rpt-context.tsx
@@ -5,7 +5,7 @@ import { useState } from 'react';
 import { MainMenu } from "@/features/menu/menu";
 
 export const RptContext = () => {
-    const [isOpen, setIsOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(true);
     return (
         <MainMenu isOpen={isOpen} setIsOpen={setIsOpen} />
     )
