a413bc13663cd51c346faf229350324639f0cc2d
Giulio Chessa <giulio.chessa@eni.com>
2023-09-05 11:32:08 +0200

    Linting error: no async for useState

--- src/app/reporting/layout.tsx
+++ src/app/reporting/layout.tsx
@@ -11,7 +11,7 @@ export const metadata = {
   description: AI_NAME,
 };
 
-export default async function RootLayout({
+export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
