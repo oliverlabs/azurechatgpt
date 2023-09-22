df2f57f8b41ea810b559881dcbf76ed8752dc971
giuliohome <giuliohome@users.noreply.github.com>
2023-09-22 15:05:18 +0000

    Feature: Allow users to edit chat thread title

--- src/features/chat/chat-menu/chat-menu.tsx
+++ src/features/chat/chat-menu/chat-menu.tsx
@@ -17,22 +17,35 @@ interface Prop {
 export const ChatMenu = (prop:Prop) => { 
   const [items, setItems] = useState<ChatThreadModel[]>([]);
 
-  // Fetch data when the component mounts
+  const [refresh, setRefresh] = useState(true);
+  // Fetch data when the component mounts and refreshes
   useEffect(() => {
     const fetchData = async () => {
+      console.log("fetching items")
       const data = await FindAllChatThreadForCurrentUser();
       setItems(data);
     };
 
     fetchData();
-  }, []);
+  }, [refresh]);
 
+  const doRefresh = () => {
+    console.log("should refresh")
+    setRefresh(!refresh);
+  }
+ 
   return (
     <div>
       <Button size={"icon"} onClick={() => prop.setIsOpen(!prop.isOpen)} >
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"  width="12" height="12" stroke="currentColor">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+        {prop.isOpen ? 
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" width="12" height="12" stroke="currentColor">
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 4v16M12 4v16M18 4v16" />
         </svg>
+        : 
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" width="12" height="12" stroke="currentColor"> 
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" /> 
+        </svg>
+        }
       </Button>
       {prop.isOpen ? 
         <Menu>
@@ -40,7 +53,7 @@ export const ChatMenu = (prop:Prop) => {
             <NewChat />
         </MenuHeader>
         <MenuContent>
-          <MenuItems menuItems={items} />
+          <MenuItems refresh={doRefresh} menuItems={items} />
         </MenuContent>
         <MenuFooter>
           <div className="flex flex-col gap-3">
